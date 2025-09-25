a4174f3642ad5aacfa1a160a32dafc075a0105a4
OrwinZavaleta <orwinzavaleta@gmail.com>
2025-09-25 09:16:51 +0200

    Se realizo el ejercicio 10

---
+++ dwec_t03p01_zavaleta_orwin/js/ejercicio10.js
@@ -0,0 +1,8 @@
+console.log("T03P01 - Ejercicio 10");
+
+let num = 12.234;
+
+console.log(Math.ceil(num)); //ceil redondea al numero superior
+console.log(Math.floor(num)); // floor redondea al numero inferior o lo trunca
+console.log(Math.round(num)); //  dedondea, mayor de 5 va al superior
+
