918f4e2f06187d3f6c29b1c2b72ff9943c3ae0e3
OrwinZavaleta <orwinzavaleta@gmail.com>
2025-10-02 12:41:44 +0200

    ejercicio 5 realizado

---
+++ dwec_t03p03_zavaleta_orwin/js/ejercicio05.js
@@ -0,0 +1,12 @@
+console.log("T03P03 - Ejercicio 05");
+
+function orderAlfabeticamente(array, orden) {
+    if (orden == "asc") {
+        return array.toSorted();
+    } else if (orden == "desc") {
+        return array.toSorted((a, b) => b - a); // en este caso, si da positivo b va primero, si da negativo a va primero
+    }
+}
+
+console.log(orderAlfabeticamente([4, 7, 5, 6], "asc"));
+console.log(orderAlfabeticamente([4, 7, 5, 6], "desc"));
