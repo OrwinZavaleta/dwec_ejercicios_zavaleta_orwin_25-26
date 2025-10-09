f82e6369ad86fa1487da50b92cb341b39980abd2
OrwinZavaleta <orwinzavaleta@gmail.com>
2025-10-09 11:13:26 +0200

    Ejercicio 11 completado con las modificaciones

--- dwec_t03p03_zavaleta_orwin/js/ejercicio11.js
+++ dwec_t03p03_zavaleta_orwin/js/ejercicio11.js
@@ -1,6 +1,6 @@
 console.log("T03P03 - Ejercicio 11");
 
-const VALORES = ["piedra", "papel", "tijera", "lagarto", "spock"];
+const VALORES = new Set(["piedra", "papel", "tijera", "lagarto", "spock"]);
 
 function pedirJugadas() {
     const array = [];
@@ -9,9 +9,24 @@ function pedirJugadas() {
 
     for (let i = 0; i < 5; i++) {
         do {
-            jugada = prompt("ingrese su jugada " + (i + 1));
+            jugada = prompt(`
+                Ingrese el numero o la jugada:
+                1. piedra
+                2. papel
+                3. tijera
+                4. lagarto
+                5. spock
 
-            if (VALORES.indexOf(jugada) != -1) {
+                Ingrese su jugada ${i + 1}
+                `);
+
+            if (!isNaN(jugada)) {
+                jugada = darNumeroJugadaEquivalente(jugada);
+            } else {
+                jugada.toLocaleLowerCase();
+            }
+
+            if (VALORES.has(jugada)) {
                 array.push(jugada);
             } else {
                 jugada = null;
@@ -20,6 +35,28 @@ function pedirJugadas() {
     }
 
     return array;
+}
+
+function darNumeroJugadaEquivalente(num) {
+    let aux = null;
+    switch (Number(num)) {
+        case 1:
+            aux = "piedra";
+            break;
+        case 2:
+            aux = "papel";
+            break;
+        case 3:
+            aux = "tijera";
+            break;
+        case 4:
+            aux = "lagarto";
+            break;
+        case 5:
+            aux = "spock";
+            break;
+    }
+    return aux;
 }
 
 function compararDosJuegos(array1, array2) { // Retorna -1 si gana el primero, 0 si empate, y 1 si el segundo gana
@@ -67,24 +104,42 @@ function compararDosValores(val1, val2) { // Retorna -1 si gana el primero, 0 si
     }
 }
 
-let jugador1 = "Anonumus1";
-let jugador2 = "Anonimus2";
+function generarJugadasAleatorias() {
+    const array = [];
 
-const array1 = [];
-const array2 = [];
+    for (let i = 0; i < 5; i++) {
+        array.push(darNumeroJugadaEquivalente(Math.random() * 5 + 1));
+    }
+
+    return array;
+}
+// =========================================================
+// ================== Inicio del programa ==================
+// =========================================================
+
+const JUGADORES = new Map();
+
+let jugador1 = null;
+let jugador2 = null;
 
 jugador1 = prompt("Ingrese el nombre del jugador 1");
+JUGADORES.set(jugador1, pedirJugadas());
 
-array1.push(...pedirJugadas());
+do {
+    jugador2 = prompt("Ingrese el nombre del jugador 2");
+    if (!JUGADORES.has(jugador2)) {
+        let confirmacion = prompt("desea jugar contra la maquina? (s/n) (defecto n)").toLowerCase();
+        if (confirmacion == "s") {
+            JUGADORES.set(jugador2, generarJugadasAleatorias());
+        } else {
+            JUGADORES.set(jugador2, pedirJugadas());
+        }
+    } else {
+        jugador2 = null;
+    }
+} while (jugador2 == null);
 
-jugador2 = prompt("Ingrese el nombre del jugador 2");
-
-array2.push(...pedirJugadas());
-
-// console.log(jugador1 + "  " + array1);
-// console.log(jugador2 + "  " + array2);
-
-let [resul, cantGanadas] = compararDosJuegos(array1, array2);
+let [resul, cantGanadas] = compararDosJuegos(JUGADORES.get(jugador1), JUGADORES.get(jugador2));
 
 switch (resul) {
     case -1:
