11e0e8b2d1858a1bb35ad2e3e25b6074bd54d433
OrwinZavaleta <orwinzavaleta@gmail.com>
2025-10-27 20:58:41 +0100

    Ejercicio 1 de objetos completado

--- dwec_t04p01_zavaleta_orwin/js/ejercicio01_alumno.js
+++ dwec_t04p01_zavaleta_orwin/js/ejercicio01_alumno.js
@@ -1,6 +1,6 @@
 console.log("T04P01 - Ejercicio 0X");
 
-function validarFecha(fecha) { // la fecha como string
+/* function validarFecha(fecha) { // la fecha como string
     let fechaSeparda;
     let esValida = true;
 
@@ -50,54 +50,66 @@ function validarFecha(fecha) { // la fecha como string
     return esValida;
 }
 
-function calcularEdad(dia, mes, anyo) {
+function calcularEdadFuera(dia, mes, anyo) {
     const fecha = new Date(anyo, mes - 1, dia);
 
     let edad = new Date() - fecha;
 
     return edad / (1000 * 60 * 60 * 24 * 365);
 }
-
+ */
 function Alumno(dni, nombre, fechaNacimiento, sexo) {
-    this.dni = dni;
-    this.nombre = nombre;
-    this.notaFinal = null;
-    this.fechaNacimiento = fechaNacimiento;
-    this.sexo = sexo;
+    this._dni = dni;
+    this._nombre = nombre;
+    this._notaFinal = null;
+    this._fechaNacimiento = fechaNacimiento;
+    this._sexo = sexo;
 
     this.mostrarInformacion = function () {
         console.log(`Dni: ${this._dni},
                  Nombre: ${this._nombre}, 
                  edad: ${this._edad}, 
                  fechaNacimiento: ${this._fechaNacimiento}, 
-                 tri1: ${this._tri1},
-                 tri2: ${this._tri2},
-                 tri3: ${this._tri3},
                  notaFinal: ${this._notaFinal},
                  sexo: ${this._sexo}`);
 
     };
 
-    this.calcularEdad = function () {
+    this.calcularEdad = function () { // me llega bien hecho (2025-12-31)
+        let fechaNac = new Date(this._fechaNacimiento);
 
+        let fechaAct = new Date();
+        let aux = Math.floor((fechaAct - fechaNac) / (1000 * 60 * 60 * 24 * 365));
+        return aux;
     }
 
-    this.edad = this.calcularEdad(); // TODO: revisar
+    this._edad = this.calcularEdad();
 
     this.calcularNota = function () {
-        this.notaFinal = this.tri1 + this.tri1 + this.tri1;
+        return (this.tri1 + this.tri2 + this.tri3) / 3;
     }
 
-    this.cambiarNotas = function (n1, n2, n3) {
-        // asigna las notas;
-    }
+    this.cambiarNotas = function (tri1, tri2, tri3) {
+        this.tri1 = tri1;
+        this.tri2 = tri2;
+        this.tri3 = tri3;
+
+        this._notaFinal = this.calcularNota();
+    };
 
     this.comparar = function (alumno) {
         // 1 si el this es mayor, -1 si el parametro es mayr, y 0 si es igual
+        if (alumno.notaFinal < this.notaFinal) {
+            return 1;
+        } else if (alumno.notaFinal == this.notaFinal) {
+            return 0;
+        } else {
+            return -1;
+        }
     }
 
     this.estaAprobado = function () {
-        // Si es mayor de 5 devuelve true
+        return (this.notaFinal >= 5);
     }
 }
 
@@ -179,8 +191,3 @@ Object.defineProperty(Alumno.prototype, "sexo", { // ya hace la validacion
         }
     },
 });
-
-const alum1 = new Alumno(123, "pepe", 12.3, 2, 2, 2, "m");
-alum1.dni = 34563
-console.log(alum1.dni);
-alum1.mostrarInformacion();
--- dwec_t04p01_zavaleta_orwin/js/ejercicio01_principal.js
+++ dwec_t04p01_zavaleta_orwin/js/ejercicio01_principal.js
@@ -1,1 +1,23 @@
 console.log("T04P01 - Ejercicio 0X");
+
+
+
+function funcionPrueba1() {
+    const alum1 = new Alumno(123, "pepe", "2007-02-23", "m");
+    const alum2 = new Alumno(345, "ana", "2003-01-13", "f");
+
+
+    alum1.mostrarInformacion();
+    alum2.mostrarInformacion();
+
+    console.log(alum2.cambiarNotas(3, 5, 9));
+    alum1.mostrarInformacion();
+    console.log(alum1.cambiarNotas(4, 7, 9));
+    alum2.mostrarInformacion();
+    console.log(alum1.comparar(alum2));
+    console.log(alum1.estaAprobado());
+    console.log(alum2.estaAprobado());
+}
+
+
+funcionPrueba1();
